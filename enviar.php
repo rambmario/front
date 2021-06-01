0ef5e082ff475242f43d275161e2770e7d8ba1b4
Mario Ramb <marioramb@gmail.com>
2021-06-01 19:16:03 -0300

    proyecto inicial front

---
+++ enviar.php
@@ -0,0 +1,33 @@
+ <?php
+
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$telefono = $_POST['telefono'];
+$asunto = $_POST['asunto'];
+$empresa = $_POST['mensaje'];
+
+$header  = 'MIME-Version: 1.0' . "\r\n";
+$header .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$header .= 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+//$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Asunto: " . $asunto . " \r\n";
+$mensaje .= "Teléfono: " . $telefono . " \r\n";
+$mensaje .= "Mensaje: " . $empresa . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'marioramb@gmail.com';
+$asunto = 'Mensaje de mi sitio web';
+
+if (mail($para, $asunto, utf8_decode($mensaje), $header)){
+    echo "<script type='text/javascript'>alert('Tu mensaje ha sido enviado exitosamente');</script>";
+    echo "<script type='text/javascript'>window.location.href='index.html';</script>";
+} else{
+    echo error_get_last()['message'];
+}
+
+?>
